2dae9ea5bd99844c522fbae76dad83ed0fb3a533
Tecspek <help.tecspek@gmail.com>
2022-07-01 18:08:37 +0530

    [ADD] sale order and invoice show in app

--- funeral_manager/models/product_category.py
+++ funeral_manager/models/product_category.py
@@ -8,7 +8,14 @@ class ProductCategory(models.Model):
     variant_2 = fields.Boolean('Variant 2')
     variant_3 = fields.Boolean('Variant 3')
 
+
 class ProductAttributeValue(models.Model):
     _inherit = 'product.attribute.value'
 
     variant_price = fields.Float('')
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    funeral_id = fields.Many2one('funeral.management')
