a203dfb86e79cf8736df3ee5c7984cc6ea06279b
Trần Đăng Tuấn <Tuan.tran@bnksolution.com>
2022-10-20 11:12:20 +0700

    Update button in/so to fu. Resolve #30385

---
+++ funeral_manager/models/sale_order.py
@@ -0,0 +1,19 @@
+from odoo import models, fields, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    funeral_id = fields.Many2one('funeral.management')
+
+
+    def action_view_funeral(self):
+        self.ensure_one()
+        return {
+            'name': _('Funeral Management'),
+            'view_mode': 'form',
+            'res_model': 'funeral.management',
+            'res_id': self.funeral_id.id,
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+        }
